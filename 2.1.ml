3c2e91e8c6675ea82567a2d61a68fdb213f24308
rjmacarthy <richardmacarthy@hotmail.com>
2024-04-14 23:36:09 +0100

    initial commit

---
+++ 2.1.ml
@@ -0,0 +1,16 @@
+1337;; (* comment *)
+(1337: int);;
+
+(* boolean operators *)
+true;;
+false;; 
+
+1337 > 1000;; (* comparison operator *)
+
+"big";; (* string literal *)
+
+"hello" ^ "world";; (* string concatenation *)
+
+1 * 1;; (* multiplication *)
+
+2.0 *. 3.14;; (* float multiplication *)
